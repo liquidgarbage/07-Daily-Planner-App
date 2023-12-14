81b289e73185276e2eb2672b596a61cc9b999fb2
liquidgarbage <128057494+liquidgarbage@users.noreply.github.com>
2023-12-14 18:36:52 +0000

    if and dataset

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # 07-Daily-Planner-App
 Daily Planner App
+
+
+
+
+
+https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/dataset
--- script.js
+++ script.js
@@ -1,17 +1,6 @@
 const saveBtn = $(".saveBtn");
 const textArea = $(".description");
 const timeBlock = $(".time-block");
-const blockHour = [
-  "#9",
-  "#10",
-  "#11",
-  "#12",
-  "#13",
-  "#14",
-  "#15",
-  "#16",
-  "#17",
-];
 
 // Day display
 function displayCurrentDay() {
@@ -21,22 +10,28 @@ function displayCurrentDay() {
 displayCurrentDay();
 
 // Timeblock display color
-var currentHour = dayjs().hour();
+// want it to color code past present and future, comparing current time to the time blocks
+
+var currentHour = dayjs().format("H");
+console.log(currentHour);
 
 function colorCodeTimeblocks() {
-  for (var i = 0; i < blockHour.length; i++) {
-    if (currentHour > blockHour[i]) {
-      timeBlock[i].addClass("past");
-    } else if (currentHour < blockHour[i]) {
-      timeBlock[i].addClass("future");
-    } else {
-      timeBlock[i].addClass("present");
+  for (var i = 0; i < timeBlock.length; i++) {
+    if (timeBlock[i].dataset.time == currentHour) {
+      timeBlock[i].classList.remove("past");
+      timeBlock[i].classList.remove("future");
+      timeBlock[i].classList.add("present");
+    } else if (timeBlock[i].dataset.time > currentHour) {
+      timeBlock[i].classList.remove("past");
+      timeBlock[i].classList.remove("present");
+      timeBlock[i].classList.add("future");
+    } else if (timeBlock[i].dataset.time < currentHour) {
+      timeBlock[i].classList.remove("present");
+      timeBlock[i].classList.remove("future");
+      timeBlock[i].classList.add("past");
     }
   }
 }
-
-console.log(currentHour);
-console.log(blockHour);
 
 colorCodeTimeblocks();
 
